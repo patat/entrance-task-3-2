a4d48b72a12be4642f6e2d06611b1f03ac6287d4
Patat <nemnogosloven@gmail.com>
2018-08-11 20:42:40 +0300

    feat: add appendGeneratedInput to save generated input to json files

--- src/test/utils.test.js
+++ src/test/utils.test.js
@@ -1,33 +1,66 @@
 import { expect } from 'chai';
+import fs from 'fs';
 
 import {
-  loadData
+  loadData,
+  appendGeneratedInput
 } from '../utils';
 
-describe('loadData', function() {
-  it('shoud return contents of a .json file', function test() {
-    const expected = [
-      {
+describe('utils', function() {
+  describe('loadData', function() {
+    it('shoud return contents of a .json file', function test() {
+      const expected = [
+        {
+            num: 1,
+            word: "One"
+        },{
+            num: 2,
+            word: "Two"
+        },{
+            num: 3,
+            word: "Three"
+        },{
+            num: 4,
+            word: "Four"
+        },{
+            num: 5,
+            word: "Five"
+        },{
+            num: 0,
+            word: "Zero"
+        }
+      ];
+      expect(loadData('ref.json')).to.deep.equal(expected);
+    });
+  });
+
+  describe('appendGeneratedInput', function() {
+    let file;
+    it('appends data to existing json file or creates new one', function test() {
+      file = 'src/test/appendedData.json';
+      const data = [
+        {
           num: 1,
           word: "One"
-      },{
+        },
+        {
           num: 2,
           word: "Two"
-      },{
+        },
+        {
           num: 3,
           word: "Three"
-      },{
-          num: 4,
-          word: "Four"
-      },{
-          num: 5,
-          word: "Five"
-      },{
-          num: 0,
-          word: "Zero"
-      }
-    ];
-    expect(loadData('ref.json')).to.deep.equal(expected);
+        }
+      ];
+      data.forEach((item, index) => {
+        expect(() => appendGeneratedInput(file, item)).to.not.throw();
+        const fileContents = loadData(file);
+        expect(fileContents).to.deep.equal(data.slice(0, index + 1));
+      });
+    });
+    afterEach(function() {
+      fs.unlinkSync(file);
+    });
   });
 });
 
--- src/utils.js
+++ src/utils.js
@@ -1,5 +1,17 @@
 import fs from 'fs';
 
 export function loadData(file) {
-  return JSON.parse(fs.readFileSync(file));
+  return JSON.parse(fs.readFileSync(file, { encoding: 'utf8' }));
+}
+
+export function appendGeneratedInput(file, input) {
+  let fileContents;
+  try {
+    fileContents = loadData(file);
+  } catch(ex) {
+    fileContents = [];
+    fs.writeFileSync(file, JSON.stringify(fileContents));
+  }
+  fileContents.push(input);
+  fs.writeFileSync(file, JSON.stringify(fileContents));
 }
